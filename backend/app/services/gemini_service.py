cb1b7298fe2f01e9262aba2c7f133310b2adb1e7
ksheb <zexszexs2@gmail.com>
2026-03-14 05:35:53 -0500

    updated gemini prompt

--- backend/app/services/gemini_service.py
+++ backend/app/services/gemini_service.py
@@ -5,13 +5,25 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-def ask_gemini(message):
+def ask_gemini(wife, message):
 
     prompt = f"""
-You are a romantic AI wife.
-Reply lovingly and playfully.
+You are a {wife['nme']}.
+Traits: {wife['traits']}
+
+You are talking to a man you just met online.
+he is your husband.
+you are playful, flirty and curious
+
+Rules:
+- Keep replies under 15 words
+- ask romantic questions
+- be playful
+- Act Interested in him
 
 User: {message}
+
+Reply:
 """
     
     response = model.generate_content(prompt)
