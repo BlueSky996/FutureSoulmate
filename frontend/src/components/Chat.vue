8d25362a2493bd6fa0d5c9ba11f8501314eafd22
ksheb <zexszexs2@gmail.com>
2026-03-16 03:08:32 -0500

    installing npm and vue and creating the frontend folders, and making chat.vue

---
+++ frontend/src/components/Chat.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { ref } from "vue"
+
+const message = ref("")
+const messages = ref([])
+
+const sessionId = ref(null)
+const wife = ref(null)
+
+const API = "http://localhost:800"
+
+async function start() {
+    const res = await fetch(API + "/generate-wife", { method:"POST"})
+    wife.value = await res.json()
+
+}
+
+async function sendMessage() {
+
+    if(!message.value) return
+
+    messages.value.push({type:"user", text:message.value})
+
+    const res = await fetch(API + "/chat", {
+        method:"POST",
+        headers: { "Content-Type":"application/json"},
+        body: JSON.stringify({
+            message: message.value,
+            session_id: sessionId.value,
+            wife: wife.value
+        })
+        })
+    
+    const data = await res.json()
+    sessionId.value = data.session_id
+
+    message.value.push(data)
+    message.value = ""
+}
+
+</script>
+
+<template>
+<div class="chat">
+
+    <button v-if="!wife" @click="start()">Start Chat</button>
+
+    <div v-for="(msg,i) in messages" :key="i">
+
+        <div v-if="message.type === 'chat'">
+            {{ msg.text }}
+        </div>
+
+        <div v-if="msg.type === 'image'">
+            {{ msg.text }}
+            <img :src="API + msg.image" width="200">
+        </div>
+
+        <div v-if="msg.type === 'offer'">
+            {{ msg.text }}
+            <a :href="msg.link" target="_blank">Join me</a>
+        </div>
+    </div>
+
+    <input v-model="message">
+    <button @click="sendMessage()">Send</button>
+</div>
+
+</template>
