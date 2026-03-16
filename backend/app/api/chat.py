bcb7ca8d948125b3932e85f57e577fee6dd918e6
ksheb <zexszexs2@gmail.com>
2026-03-15 22:34:26 -0500

    fixing syntax on chat.py and remove duplicated gemini calls

--- backend/app/api/chat.py
+++ backend/app/api/chat.py
@@ -9,19 +9,19 @@ OFFER_LINK = "https://www.google.com"
 @router.post("/chat")
 async def chat(req: dict = Body(...)):
     sid = req.get("session_id") or create_session()
-    user_msg = req.get["message"]
+
+    user_msg = req.get("message")
     wife_data = req["wife"]
 
-    reply = ask_gemini(wife_data, user_msg)
+    session = get_session(sid)
     increment_messages(sid)
-
     messages = session["messages"]
 
     # LEVEL 1
     if messages == 3:
         return {
             "type": "image",
-            "image": "/images/wife1.jps",
+            "image": "/images/wife1.jpg",
             "text": "I just took this for you ❤️ Do you like me?"
         }
     
@@ -37,4 +37,6 @@ async def chat(req: dict = Body(...)):
     reply = ask_gemini(wife_data, user_msg)
 
     return {"reply": "chat", 
-            "text": reply}
+            "text": reply,
+            "session_id": sid
+            }
